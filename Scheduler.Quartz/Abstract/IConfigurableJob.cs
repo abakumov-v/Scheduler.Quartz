6f752b671bc772048f2461e1371537f1d3378b92
abakumov-v <6594815+abakumov-v@users.noreply.github.com>
2019-10-10 09:42:01 +0300

    rename properties for IConfigurableJob

--- Scheduler.Quartz/Abstract/IConfigurableJob.cs
+++ Scheduler.Quartz/Abstract/IConfigurableJob.cs
@@ -4,9 +4,9 @@ namespace Scheduler.Quartz.Abstract
 {
     public interface IConfigurableJob : IJob
     {
-        IJobDetail ConfigJobDetail();
-        ITrigger ConfigTrigger();
-        JobBuilder ConfigJobBuilder();
-        TriggerBuilder ConfigTriggerBuilder();
+        IJobDetail CreateJobDetail();
+        ITrigger CreateTrigger();
+        JobBuilder CreateJobBuilder();
+        TriggerBuilder CreateTriggerBuilder();
     }
 }
---
+++ examples/Core.Jobs/SomeConfigurableJob.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+using Quartz;
+using Scheduler.Quartz.Abstract;
+
+namespace Core.Jobs
+{
+    public class SomeConfigurableJob : IConfigurableJob
+    {
+        public Task Execute(IJobExecutionContext context)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IJobDetail CreateJobDetail()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public ITrigger CreateTrigger()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public JobBuilder CreateJobBuilder()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public TriggerBuilder CreateTriggerBuilder()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
